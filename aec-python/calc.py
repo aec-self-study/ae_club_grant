f2941cc771c45db22f9ea224ceafc8910afddb3d
ghgeist <granthgeist@gmail.com>
2022-12-01 01:03:51 +0000

    add unit testing

--- aec-python/calc.py
+++ aec-python/calc.py
@@ -12,16 +12,19 @@ add.add_argument("ints_to_sum", nargs=2, type=int)
 sub = subparsers.add_parser("sub", help = "Subtract integers")
 sub.add_argument("ints_to_sub", nargs=2, type=int)
 
-args = parser.parse_args()
-
-if args.command == "add":
-    our_sum = sum(args.ints_to_sum)
-    print(f"the sum of values is {our_sum}")
-
-if args.command == 'sub':
-    our_sub = args.ints_to_sub[0] - args.ints_to_sub[1]
+def aec_subtract(ints_to_sub):
+    our_sub = ints_to_sub[0] - ints_to_sub[1]
+    if our_sub < 0:
+        our_sub = 0
     print(f"the sum of values is {our_sub}")
+    return(our_sub)
 
+if __name__ == "__main__":
+    args = parser.parse_args()
 
-# our_sum = sum(args.ints_to_sum)
-# print(f"the sum of values is {our_sum}")
+    if args.command == "add":
+        our_sum = sum(args.ints_to_sum)
+        print(f"the sum of values is {our_sum}")
+
+    if args.command == 'sub':
+        aec_subtract(args.ints_to_sub)
